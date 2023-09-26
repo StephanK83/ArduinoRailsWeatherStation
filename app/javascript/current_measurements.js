47d8388df9d104c52fcbd2ea401094cbef315676
StephanK83 <Stephan.knorn@berlin101.de>
2023-09-26 14:29:27 +0200

    added graph-view-functionality (bugfixing7)

--- app/javascript/current_measurements.js
+++ app/javascript/current_measurements.js
@@ -1,8 +1,11 @@
 console.log("Current_measurements.js loaded!");
 document.addEventListener("DOMContentLoaded", () => {
   const chartElement = document.getElementById('temperatureChart');
-  const temperatures = JSON.parse(chartElement.dataset.temperatures);
-  const timestamps = JSON.parse(chartElement.dataset.timestamps);
+  // const temperatures = JSON.parse(chartElement.dataset.temperatures);
+  // const timestamps = JSON.parse(chartElement.dataset.timestamps);
+  const temperatures = JSON.parse(canvas.dataset.temperatures);
+  const timestamps = JSON.parse(canvas.dataset.timestamps);
+
 
   const ctx = chartElement.getContext('2d');
   const temperatureChart = new ChartJS.Chart(ctx, {
