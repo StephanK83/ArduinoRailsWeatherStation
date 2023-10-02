c286aa2a41eb2061eeec1bd40665cfb64bf73a54
StephanK83 <Stephan.knorn@berlin101.de>
2023-10-02 23:08:00 +0200

    jscript bugfixing 5

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // import "@hotwired/turbo-rails"
 import "controllers"
-import "https://cdn.jsdelivr.net/npm/chart.js"
-import "https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"
+// import "https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"
 import "https://cdn.jsdelivr.net/npm/chart.js"
 // import "./current_measurements.js"
 
