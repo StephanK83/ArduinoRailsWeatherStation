feb61efcd3fcbdeca20e4230220bc59d069326b0
StephanK83 <Stephan.knorn@berlin101.de>
2023-11-10 16:32:10 +0100

    solved probles, added buttons, deleted charts, fixed database

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'about', to: 'about#index', as: 'about'
   get 'arduino_code', to: 'arduino_code#index', as: 'arduino_code'
   get 'arduino_setup', to: 'arduino_setup#index', as: 'arduino_setup'
+  get 'forecast', to: 'forecast#index', as: 'forecast'
+  get 'review', to: 'review#index', as: 'review'
   resources :current_measurements, only: [:create, :index]
 end
